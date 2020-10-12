0f4e2e60daf4ea7fe0653c322e049cf44fcc44c6
Baezus <elias@baez.us>
2020-10-12 17:30:27 -0400

    CRUD complete for Comments model.

--- controllers/commentCTRL.js
+++ controllers/commentCTRL.js
@@ -82,5 +82,41 @@ router.get('/:commentId/edit', (req, res) => {
      });
 });
 
+router.delete('/:commentId', (req, res) => {
+     const commentId = req.params.commentId;
+
+     db.Comment.findByIdAndDelete(commentId, (err) => {
+          if (err) return console.log(err);
+
+          db.Place.findOne({'comments': commentId}, (err, foundPlace) => {
+               if (err) return console.log(err);
+
+               foundPlace.comments.remove(commentId);
+               foundPlace.save((err, updatedPlace) => {
+                    if (err) return console.log(err);
+
+                    res.redirect('/comments');
+               })
+          })
+
+
+     });
+});
+
+//PUT update
+
+router.put('/:commentId', (req, res) => {
+     db.Comment.findByIdAndUpdate(
+          req.params.commentId,
+          req.body,
+          {new:true},
+          (err, updatedComment) => {
+               if (err) return console.log(err);
+
+               res.redirect(`/comments/${updatedComment._id}`);
+          }
+     );
+});
+
 
 module.exports = router;
